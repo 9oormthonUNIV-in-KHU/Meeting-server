ea74cf12009aeb2177f532097d662881f9abfee0
seongho5356 <kjeng7897@gmail.com>
2025-04-12 02:29:05 +0900

    feat: (#1) 포트원 설정 정보를 작성한다

---
+++ src/main/java/org/groomUniv/meet/common/config/PortOneConfig.java
@@ -0,0 +1,21 @@
+package org.groomUniv.meet.common.config;
+
+import com.siot.IamportRestClient.IamportClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PortOneConfig {
+
+    @Value("${imp.key}")
+    private String apiKey;
+
+    @Value("${imp.secret_key}")
+    private String secretKey;
+
+    @Bean
+    public IamportClient iamportClient() {
+        return new IamportClient(apiKey, secretKey);
+    }
+}
