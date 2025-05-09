613c9b47c285f54b03040d5004fed0d27192ac55
sunwoo <sswsjw1234@gmail.com>
2025-05-10 01:11:49 +0900

    feat: (#8) add createUserDetails

--- src/main/java/org/groomUniv/meet/common/security/CustomUserDetailsService.java
+++ src/main/java/org/groomUniv/meet/common/security/CustomUserDetailsService.java
@@ -2,7 +2,9 @@ package org.groomUniv.meet.common.security;
 
 import lombok.RequiredArgsConstructor;
 import org.groomUniv.meet.oauth.entity.Member;
+import org.groomUniv.meet.oauth.enums.Role;
 import org.groomUniv.meet.oauth.repository.MemberRepository;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -15,9 +17,19 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Member member = memberRepository.findByEmail(username)
+        return memberRepository.findByEmail(username)
+                .map(this::createUserDetails)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
 
-        return new CustomUserDetails(member);
+
+    private UserDetails createUserDetails(Member member) {
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRoles().stream()
+                        .map(Enum::name)
+                        .toArray(String[]::new))
+                .build();
     }
 }
