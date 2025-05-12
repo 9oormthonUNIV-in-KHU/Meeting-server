3913885845e1f5e215a9f3d16ecbe7bd3f2dcda4
sunwoo <sswsjw1234@gmail.com>
2025-05-12 20:10:29 +0900

    feat: (#8) add NO_AUTH_PATHS

--- src/main/java/org/groomUniv/meet/common/security/JwtAuthenticationFilter.java
+++ src/main/java/org/groomUniv/meet/common/security/JwtAuthenticationFilter.java
@@ -12,15 +12,34 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
 
 import java.io.IOException;
+import java.util.List;
+
+import static org.aspectj.weaver.tools.cache.SimpleCacheFactory.path;
 
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilterBean {
     private final JwtTokenProvider jwtTokenProvider;
 
+
+    private static final List<String> NO_AUTH_PATHS = List.of(
+            "/api/member/signup",
+            "/api/member/login"
+    );
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        String token = resolveToken((HttpServletRequest) servletRequest);  // header에서 jwt 토큰을 추출
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
 
+        String path = request.getServletPath();
+        System.out.println(path);
+        // 인증이 필요 없는 경로는 필터 패스
+        if (NO_AUTH_PATHS.contains(path)) {
+            System.out.println("No auth needed for path: " + path);
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        String token = resolveToken((HttpServletRequest) servletRequest);  // header에서 jwt 토큰을 추출
         if (token != null && jwtTokenProvider.validateToken(token)) {   // 토큰이 존재하고 유효할 경우
             Authentication authentication = jwtTokenProvider.getAuthentication(token);  // 토큰에서 Authentication 객체를 얻어
             SecurityContextHolder.getContext().setAuthentication(authentication);   // Spring SecurityContext에 저장
